3237b7913e210ab7e9a6fe04f8e6f24c28aea3c2
beginfish <juexian4eagle@163.com>
2019-09-09 10:21:30 +0800

    bubble

--- src/main/java/com/bamdow/algorithm/BubbleSort.java
+++ src/main/java/com/bamdow/algorithm/BubbleSort.java
@@ -2,6 +2,21 @@ package com.bamdow.algorithm;
 
 import java.util.Arrays;
 
+/**
+ * 冒泡排序
+ * 
+ * <P>原理：</P>
+ * 相邻2个元素比较,较大的元素向后冒泡
+ * n个元素经过n-1趟比较之后所有元素归位
+ * <P>思路：</P>
+ *第1趟比较最大的元素被排到倒数最一位
+ *第2趟比较第二大的元素被排到倒数第二位
+ *~~
+ *第n-2趟第三小的元素被排到顺数最三位
+ *第n-1趟最小的二个的元素被排到正确的位置
+ * @author Bamboo
+ *
+ */
 public class BubbleSort {
 
 	public static void sort(int[] data){
